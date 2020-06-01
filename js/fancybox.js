90e3ab5bb4f8cdc53e845c354500fad274571854
Jayashri <jayashri1394@gmail.com>
2020-06-01 03:01:53 +0200

    iframe is added for individual projects & text updation

---
+++ js/fancybox.js
@@ -0,0 +1,44 @@
+$("#various5").fancybox({
+    'width'             : '75%',
+    'height'            : '75%',
+    'autoScale'         : false,
+    'transitionIn'      : 'none',
+    'transitionOut'     : 'none',
+    'type'              : 'iframe'
+});
+
+$("#various6").fancybox({
+    'width'             : '75%',
+    'height'            : '75%',
+    'autoScale'         : false,
+    'transitionIn'      : 'none',
+    'transitionOut'     : 'none',
+    'type'              : 'iframe'
+});
+
+$("#various7").fancybox({
+    'width'             : '75%',
+    'height'            : '75%',
+    'autoScale'         : false,
+    'transitionIn'      : 'none',
+    'transitionOut'     : 'none',
+    'type'              : 'iframe'
+});
+
+$("#various8").fancybox({
+    'width'             : '75%',
+    'height'            : '75%',
+    'autoScale'         : false,
+    'transitionIn'      : 'none',
+    'transitionOut'     : 'none',
+    'type'              : 'iframe'
+});
+
+$("#various9").fancybox({
+    'width'             : '75%',
+    'height'            : '75%',
+    'autoScale'         : false,
+    'transitionIn'      : 'none',
+    'transitionOut'     : 'none',
+    'type'              : 'iframe'
+});
